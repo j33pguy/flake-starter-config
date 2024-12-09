1e12e2521b205f23d182e5013fcf7cee4207fa6c
j33pguy <30570813+j33pguy@users.noreply.github.com>
2024-12-09 08:51:32 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,11 @@
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+in {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
